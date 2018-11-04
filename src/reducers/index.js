38bc78caed717df2970e267736fdb40d267aba16
pablogadhi <pablogadhi@gmail.com>
2018-11-04 11:44:58 -0600

    BoilerPlate de Posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import categories, * as categorySelectors from './categories';
 import belongings, * as belongingSelectors from './belongings';
 import users, * as userSelectors from './users';
+import posts, * as postSelectors from './posts';
 
 const reducer = combineReducers({
   categories,
   belongings,
   users,
+  posts,
 });
 
 export default reducer;
@@ -19,3 +21,5 @@ export const getBelonging = (state, id) => belongingSelectors.getBelonging(state
 export const getBelongings = state => belongingSelectors.getBelongings(state.belongings);
 export const getUser = (state, id) => userSelectors.getUser(state.users, id);
 export const getUsers = state => userSelectors.getUsers(state.users);
+export const getPost = (state, id) => postSelectors.getPost(state.posts, id);
+export const getPosts = state => postSelectors.getPosts(state.posts);
